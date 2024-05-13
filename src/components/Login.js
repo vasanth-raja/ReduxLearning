160eb12d51419cc892420ef3d48942be31aa952c
vasanth-raja <vasathrajark@gmail.com>
2024-05-13 09:40:16 +0530

    dummy login and logout

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import { useDispatch } from 'react-redux'
+import { login,logout } from '../features/user';
 function Login() {
+  const dispatch=useDispatch();
   return (
     <div>
-        <button>Login</button>
+        <button onClick={()=>{dispatch(login({name:"vas",age:20,email:"vas@v"}))}}>Login</button>
+        <button onClick={()=>{dispatch(logout())}}>Logout</button>
     </div>
   )
 }
