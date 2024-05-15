f73722155a6922bcefebcad3d81c07f4aaebbd6e
vasanth-raja <vasanthrajark@gmail.com>
2024-05-15 13:19:00 +0530

    theme

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Login from './components/Login';
 import Profile from './components/Profile';
+import ChangeColor from './components/ChangeColor';
 
 function App() {
   return (
     <div className="App">
      <Profile/>
      <Login/>
+     <ChangeColor/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import reportWebVitals from './reportWebVitals';
 import {configureStore} from "@reduxjs/toolkit"
 import {Provider} from "react-redux"
 import userReducer from './features/user'
+import themeReducer from './features/theme'
 const store =configureStore({
   reducer:{
     user:userReducer,
+    theme:themeReducer
   }
 })
 const root = ReactDOM.createRoot(document.getElementById('root'));
